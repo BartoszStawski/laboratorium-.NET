be6ca188d7efdd84ccde9741a396966820b8f880
BartS <38958581+BartoszStawski@users.noreply.github.com>
2024-05-16 20:10:13 +0200

    Zadanie 6

---
+++ LAB1/Zadanie_6/Zadanie_6.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+class Zadanie_6
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj nazwę pliku źródłowego:");
+        string sourceFileName = Console.ReadLine();
+
+        Console.WriteLine("Podaj nazwę pliku docelowego:");
+        string destinationFileName = Console.ReadLine();
+
+        try
+        {
+            CopyFile(sourceFileName, destinationFileName);
+            Console.WriteLine("Plik został pomyślnie skopiowany.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Wystąpił błąd: {ex.Message}");
+        }
+    }
+
+    static void CopyFile(string sourceFileName, string destinationFileName)
+    {
+        // Otwieramy plik źródłowy do odczytu
+        using (FileStream sourceStream = new FileStream(sourceFileName, FileMode.Open))
+        {
+            // Tworzymy lub nadpisujemy plik docelowy do zapisu
+            using (FileStream destinationStream = new FileStream(destinationFileName, FileMode.Create))
+            {
+                // Kopiujemy zawartość pliku źródłowego do pliku docelowego
+                sourceStream.CopyTo(destinationStream);
+            }
+        }
+    }
+}
