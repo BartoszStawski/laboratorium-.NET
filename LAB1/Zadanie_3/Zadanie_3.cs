69e7cd9fe22c65d103334f50e86275035f8c718e
BartS <38958581+BartoszStawski@users.noreply.github.com>
2024-05-16 20:04:09 +0200

    Zadanie 3

---
+++ LAB1/Zadanie_3/Zadanie_3.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+class Zadanie_3
+{
+    static void Main(string[] args)
+    {
+        string filePath = "C:/Users/barte/Documents/wsb/4 sem/.NET/LAB1/files/sample.txt";
+
+        try
+        {
+            // Sprawdzenie, czy plik istnieje
+            if (File.Exists(filePath))
+            {
+                // Otwieranie pliku za pomocą StreamReader
+                using (StreamReader streamReader = new StreamReader(filePath))
+                {
+                    // Odczytanie zawartości pliku i wyświetlenie na konsoli
+                    string content = streamReader.ReadToEnd();
+                    Console.WriteLine("Zawartość pliku:");
+                    Console.WriteLine(content);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Plik nie istnieje.");
+            }
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("Plik nie został znaleziony.");
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine("Wystąpił błąd wejścia-wyjścia: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Wystąpił nieoczekiwany błąd: " + ex.Message);
+        }
+    }
+}
