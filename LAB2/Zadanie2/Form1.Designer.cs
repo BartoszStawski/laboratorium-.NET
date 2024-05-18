bf4456e682513a64d8d32f395c394ae2cdaf5769
BartS <38958581+BartoszStawski@users.noreply.github.com>
2024-05-18 13:29:17 +0200

    Zadanie 2

---
+++ LAB2/Zadanie2/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.TextBox textBox_Result;
+        private System.Windows.Forms.Label labelCurrentOperation;
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_Result = new System.Windows.Forms.TextBox();
+            this.labelCurrentOperation = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_Result
+            // 
+            this.textBox_Result.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.textBox_Result.Location = new System.Drawing.Point(12, 12);
+            this.textBox_Result.Name = "textBox_Result";
+            this.textBox_Result.Size = new System.Drawing.Size(260, 38);
+            this.textBox_Result.TabIndex = 0;
+            this.textBox_Result.Text = "0";
+            this.textBox_Result.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // labelCurrentOperation
+            // 
+            this.labelCurrentOperation.AutoSize = true;
+            this.labelCurrentOperation.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.labelCurrentOperation.Location = new System.Drawing.Point(12, 53);
+            this.labelCurrentOperation.Name = "labelCurrentOperation";
+            this.labelCurrentOperation.Size = new System.Drawing.Size(0, 20);
+            this.labelCurrentOperation.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.labelCurrentOperation);
+            this.Controls.Add(this.textBox_Result);
+            this.Name = "Form1";
+            this.Text = "Simple Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+            // Adding buttons dynamically
+            int startX = 12, startY = 80, buttonWidth = 60, buttonHeight = 40;
+            string[] buttonLabels = { "7", "8", "9", "/", "4", "5", "6", "*", "1", "2", "3", "-", "0", "C", "CE", "+", "=" };
+
+            for (int i = 0; i < buttonLabels.Length; i++)
+            {
+                Button button = new Button();
+                button.Size = new System.Drawing.Size(buttonWidth, buttonHeight);
+                button.Location = new System.Drawing.Point(startX + (i % 4) * buttonWidth, startY + (i / 4) * buttonHeight);
+                button.Text = buttonLabels[i];
+                button.Click += buttonLabels[i] == "=" ? new EventHandler(this.buttonEquals_Click) :
+                                buttonLabels[i] == "C" ? new EventHandler(this.buttonC_Click) :
+                                buttonLabels[i] == "CE" ? new EventHandler(this.buttonCE_Click) :
+                                "+-*/".Contains(buttonLabels[i]) ? new EventHandler(this.operator_Click) :
+                                new EventHandler(this.button_Click);
+                this.Controls.Add(button);
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ LAB2/Zadanie2/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        private double result = 0;
+        private string operation = "";
+        private bool isOperationPerformed = false;
+        private Stopwatch stopwatch;
+
+        public Form1()
+        {
+            stopwatch = Stopwatch.StartNew();
+            InitializeComponent();
+            stopwatch.Stop();
+
+            long elapsedMilliseconds = stopwatch.ElapsedMilliseconds;
+            if (elapsedMilliseconds > 1000) // Próg czasu uruchamiania (np. 1 sekunda)
+            {
+                LogEvent("Application initialization time exceeded threshold.", $"Initialization time: {elapsedMilliseconds} ms");
+            }
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if ((textBox_Result.Text == "0") || (isOperationPerformed))
+                textBox_Result.Clear();
+
+            isOperationPerformed = false;
+            Button button = (Button)sender;
+            textBox_Result.Text += button.Text;
+        }
+
+        private void operator_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if (result != 0)
+            {
+                buttonEquals_Click(sender, e); // Wywołanie metody obsługującej przycisk "="
+                operation = button.Text;
+                labelCurrentOperation.Text = result + " " + operation;
+                isOperationPerformed = true;
+            }
+            else
+            {
+                operation = button.Text;
+                result = Double.Parse(textBox_Result.Text);
+                labelCurrentOperation.Text = result + " " + operation;
+                isOperationPerformed = true;
+            }
+        }
+
+        private void buttonCE_Click(object sender, EventArgs e)
+        {
+            textBox_Result.Text = "0";
+        }
+
+        private void buttonC_Click(object sender, EventArgs e)
+        {
+            textBox_Result.Text = "0";
+            result = 0;
+            labelCurrentOperation.Text = "";
+        }
+
+        private void buttonEquals_Click(object sender, EventArgs e)
+        {
+            switch (operation)
+            {
+                case "+":
+                    textBox_Result.Text = (result + Double.Parse(textBox_Result.Text)).ToString();
+                    break;
+                case "-":
+                    textBox_Result.Text = (result - Double.Parse(textBox_Result.Text)).ToString();
+                    break;
+                case "*":
+                    textBox_Result.Text = (result * Double.Parse(textBox_Result.Text)).ToString();
+                    break;
+                case "/":
+                    textBox_Result.Text = (result / Double.Parse(textBox_Result.Text)).ToString();
+                    break;
+                default:
+                    break;
+            }
+            result = Double.Parse(textBox_Result.Text);
+            operation = "";
+            labelCurrentOperation.Text = "";
+        }
+
+        private void LogEvent(string message, string details)
+        {
+            string source = "SimpleCalculatorApp";
+            string log = "Application";
+            string eventMessage = $"{message}\n\nDetails:\n{details}";
+
+            if (!EventLog.SourceExists(source))
+            {
+                EventLog.CreateEventSource(source, log);
+            }
+
+            EventLog.WriteEntry(source, eventMessage, EventLogEntryType.Information);
+        }
+    }
+}
